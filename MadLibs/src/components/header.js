b1122657e46e9e8a16a8e9e5d6c769c3959e4d25
Cannon Farr <farrcan000@alpine.local>
2018-06-27 09:00:21 -0600

    made base components

---
+++ MadLibs/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Mad Libs</h1>
+      <p>Fill out the fields below and click generate to see the story</p>
+    </div>
+  );
+};
+
+export default Header;
