128d6b1e7b5c29d8cfbbe891df611689392d9ec4
Cannon Farr <farrcan000@alpine.local>
2018-06-28 09:01:45 -0600

    made state stuff

--- MadLibs/src/components/card.js
+++ MadLibs/src/components/card.js
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import Input from "./input";
 
 class Card extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      color: "",
+      pluralNoun: ""
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="card">
-        {Input("Color")}
-        {Input("Plural Noun")}
+        {Input("Color", this.state.color, this.handleInput, "color")}
+        {Input(
+          "Plural Noun",
+          this.state.pluralNoun,
+          this.handleInput,
+          "pluralNoun"
+        )}
       </div>
     );
   }
