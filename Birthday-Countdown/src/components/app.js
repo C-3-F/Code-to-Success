dd24461f64b64c2db6b7b9aafe5aa0b26afdf685
Cannon Farr <farrcan000@alpine.local>
2018-07-09 11:54:33 -0600

    Worked on countdown

--- Birthday-Countdown/src/components/app.js
+++ Birthday-Countdown/src/components/app.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
+import Picker from "./picker";
+import Button from "./button";
+import Clock from "./clock";
+import ChangeDate from "./changeDate";
+import { largeText } from "./largeText";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false
+    };
+  }
+
+  renderItems = function() {
+    if (this.state.active) {
+      return [
+        <Clock />,
+        ChangeDate("Change Date", () => this.setState({ active: false })),
+        largeText("04/03"),
+        <label className="grid__remaining">Remaining until your b-day</label>
+      ];
+    } else {
+      return [
+        Button("Generate Countdown", () => this.setState({ active: true })),
+        <Picker />
+      ];
+    }
+  };
+
   render() {
     return (
       <div className="grid">
+        <div className="grid__title">Birthday Countdown</div>
         <div className="grid__skew-light-one" />
         <div className="grid__skew-light-two" />
         <div className="grid__skew-light-three" />
         <div className="grid__skew-dark-one" />
         <div className="grid__skew-dark-two" />
         <div className="grid__skew-dark-three" />
+
+        {this.renderItems()}
       </div>
     );
   }
---
+++ Birthday-Countdown/src/components/largeText.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const largeText = text => {
+  return <div className="large-text">{text}</div>;
+};
